bacda58b60eef67bb14a2885b660aeed153aebe1
User <>
2023-10-01 16:07:11 +0300

    First WebGL Build

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    //private GameObject playerScore, enemyScore;
+    public TMP_Text playerScore, enemyScore;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name.Contains("Goal"))
+        {
+            transform.position = Vector3.zero;
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+        if (collision.gameObject.name.Contains("Enemy Goal"))
+        {
+            playerScore.text = (int.Parse(playerScore.text) + 1).ToString();
+        }
+        if (collision.gameObject.name.Contains("Player Goal"))
+        {
+            enemyScore.text = (int.Parse(enemyScore.text) + 1).ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private void Update()
+    {
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+
+        if (mousePos.x > 0) mousePos.x = 0;
+
+        GetComponent<Rigidbody2D>().MovePosition(mousePos);
+    }
+}
